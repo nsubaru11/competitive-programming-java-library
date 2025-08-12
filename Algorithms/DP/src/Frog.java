4ab7e7d6b7fc1af47b80c5ba3c85be8b33a23090
nsubaru11 <20051126subaru@gmail.com>
2025-08-12 17:02:18 +0900

    feat: DP問題用ソースコードと環境設定を追加
    詳細:
    - 動的計画法（DP）用のサンプルコード`Frog.java`および`Knapsack.java`を新規作成。

---
+++ Algorithms/DP/src/Frog.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+import static java.lang.Math.*;
+
+public class Frog {
+
+	private static void solve(final Scanner sc, final PrintWriter out) {
+		int n = sc.nextInt();
+		int k = sc.nextInt();
+		int[] h = new int[n];
+		for (int i = 0; i < n; i++) {
+			h[i] = sc.nextInt();
+		}
+		out.println(frogProblem(n, k, h));
+	}
+
+	public static int frogProblem(int n, int k, int[] h) {
+		int[] dp = new int[k];
+		for (int i = 1; i < min(n, k); i++) {
+			dp[i] = abs(h[i] - h[0]);
+		}
+		for (int i = k; i < n; i++) {
+			int idx = i % k;
+			int cost = abs(h[i] - h[i - 1]);
+			dp[idx] = dp[idx] + cost;
+			for (int j = 2; j <= k; j++) {
+				int idx2 = ((idx - j) % k + k) % k;
+				cost = abs(h[i] - h[i - j]);
+				dp[idx] = min(dp[idx], dp[idx2] + cost);
+			}
+		}
+		return dp[(n - 1) % k];
+	}
+
+	public static void main(String[] args) {
+		try (final Scanner sc = new Scanner(System.in);
+			 final PrintWriter out = new PrintWriter(System.out)) {
+			solve(sc, out);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+}
---
+++ Algorithms/DP/src/PartialSumProblem.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.util.*;
+import java.math.*;
+import java.util.function.*;
+
+import static java.lang.Math.*;
+import static java.util.Arrays.*;
+
+public class PartialSumProblem {
+
+	private static void solve(final Scanner sc, final PrintWriter out) {
+		int n = sc.nextInt();
+		int k = sc.nextInt();
+		int[] a = new int[n];
+		int sum = 0;
+		for (int i = 0; i < n; i++) {
+			sum += a[i] = sc.nextInt();
+		}
+		boolean f = partialSumProblem(n, a, k, sum);
+		out.println("複数回使用なし" + (f ? "Yes" : "No"));
+		boolean f2 = partialSumProblem(n, a, k);
+		out.println("複数回使用あり" + (f2 ? "Yes" : "No"));
+	}
+
+	public static boolean partialSumProblem(int n, int[] a, int k) {
+		boolean[] dp = new boolean[k + 1];
+		dp[0] = true;
+		for (int i = 0; i < n; i++) {
+			for (int j = a[i]; j <= k; j++) {
+				dp[j] |= dp[j - a[i]];
+			}
+		}
+		return dp[k];
+	}
+
+	public static boolean partialSumProblem(int n, int[] a, int k, int sum) {
+		if (sum < k) return false;
+		boolean[] dp = new boolean[k + 1];
+		dp[0] = true;
+		for (int i = 0; i < n; i++) {
+			for (int j = k; j >= a[i]; j--) {
+				dp[j] |= dp[j - a[i]];
+			}
+		}
+		return dp[k];
+	}
+
+	public static void main(String[] args) {
+		try (final Scanner sc = new Scanner(System.in);
+			 final PrintWriter out = new PrintWriter(System.out)) {
+			solve(sc, out);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+}
