d7b7295358cf58e5b204a320257b6d2ebbae13d5
nsubaru11 <20051126subaru@gmail.com>
2025-09-12 18:21:12 +0900

    feat: ExampleクラスにAVLListおよびAVLSetのサンプルロジックを追加
    詳細:
    - `Example`クラスにランダムな整数を利用した`AVLList`と`AVLSet`の使用例を追記。
    - `distinctIterator`を用いた`AVLList`のユニーク要素イテレーション処理を追加。
    - 各操作結果をコンソール出力する簡易ロジックを実装し、動作確認を容易化。

--- Data Structures/BinarySearchTree/src/Example.java
+++ Data Structures/BinarySearchTree/src/Example.java
@@ -1,5 +1,32 @@
-public class Example {
-	public static void main(String[] args) {
+import java.util.Iterator;
 
+public class Example {
+
+	public static void main(String[] args) {
+		int n = 10;
+		AVLList<Integer> list = new AVLList<>();
+		AVLSet<Integer> set = new AVLSet<>();
+		for (int i = 0; i < n; i++) {
+			int random = (int) (Math.random() * n);
+			list.add(random);
+			set.add(random);
+			System.out.print("a list:");
+			for (int j : list) {
+				System.out.print(" " + j);
+			}
+
+			System.out.print("\nb list:");
+			for (Iterator<Integer> it = list.distinctIterator(); it.hasNext(); ) {
+				int j = it.next();
+				System.out.print(" " + j);
+			}
+
+			System.out.print("\na  set:");
+			for (int j : set) {
+				System.out.print(" " + j);
+			}
+			System.out.println('\n');
+		}
 	}
+
 }
