9c7f14231e9cc750ac11388ace4c26695e55b2fb
nsubaru11 <20051126subaru@gmail.com>
2025-10-13 11:30:41 +0900

    refactor: `Example` クラスのデバッグコードを整理

--- Algorithms/Factorial/src/Example.java
+++ Algorithms/Factorial/src/Example.java
@@ -1,9 +1,8 @@
 public final class Example {
 
 	public static void main(String[] args) {
-//		PreComputedFactorials fact = new PreComputedFactorials(13L);
-//		System.out.println(fact.combination(10, 2));
-//		System.out.println(fact.combination(100000, 2));
+		PreComputedFactorials fact = new PreComputedFactorials(13);
+		System.out.println(fact.factorial(10));
 		System.out.println(FactorialUtils.factorial(5));
 	}
 
