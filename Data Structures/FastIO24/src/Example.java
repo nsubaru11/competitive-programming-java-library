ad0a11939dbd25e61a28bd107e46ffe78eddfd14
nsubaru11 <20051126subaru@gmail.com>
2025-11-11 19:37:30 +0900

    feat: `Example` クラスを追加し、FastIOの使用例を提供
    詳細:
    - `ContestScanner` と `ContestPrinter` の使用例を含む基本的なIOロジックを実装。
    - 配列、リスト、多様な型（プリミティブ型やコレクション）の出力デモを追加。
    - メイン関数内で例外処理を行い、リソースの適切な終了処理を実施。
    - クラスを `final` 化して意図しない継承を防止。

---
+++ Data Structures/FastIO24/src/Example.java
@@ -0,0 +1,31 @@
+import java.util.List;
+
+public final class Example {
+
+	// ------------------------ 定数 ------------------------
+	private static final ContestScanner sc = new ContestScanner();
+	private static final ContestPrinter out = new ContestPrinter();
+
+	// ------------------------ メインロジック ------------------------
+	private static void solve() {
+		Object[] arr = {1, "21231", 3.14159265358979323846, new int[]{1, 2, 3, 4, 5}, new String[]{"a", "b", "c", "d", "e"}, null};
+		out.print(arr);
+		out.println();
+		out.println(1, "232", new int[]{1, 2, 3, 4}, null);
+		List<Integer> list = List.of(1, 2, 3, 4, 5);
+		out.print(list, i -> i * i);
+	}
+
+	// ------------------------ main() 関数 ------------------------
+	public static void main(final String[] args) {
+		try {
+			solve();
+		} catch (final Exception e) {
+			e.printStackTrace();
+		} finally {
+			sc.close();
+			out.close();
+		}
+	}
+
+}
