221a6251a7ea3ef52e6fef2e64d52a80ebcba61a
nsubaru11 <20051126subaru@gmail.com>
2026-07-15 00:40:53 +0900

    test: 座圧ライブラリを用いたコードの問題を解いた

---
+++ test/verify/ds/arrays/Check3.java
@@ -0,0 +1,460 @@
+package verify.ds.arrays;
+
+import static java.lang.Math.*;
+import static java.util.Arrays.*;
+
+import java.util.*;
+import java.util.function.*;
+import java.util.stream.*;
+
+import lib.ds.arrays.*;
+import lib.io.*;
+
+// https://atcoder.jp/contests/abc036/tasks/abc036_c
+public final class Check3 {
+
+	// region < Constants & Globals >
+	private static final boolean DEBUG = true;
+	private static final int MOD = 998244353;
+	// private static final int MOD = 1_000_000_007;
+	private static final char[] op = new char[]{'L', 'U', 'R', 'D'};
+	private static final int[] di = new int[]{0, -1, 0, 1, -1, -1, 1, 1};
+	private static final int[] dj = new int[]{-1, 0, 1, 0, -1, 1, 1, -1};
+	private static final FastScanner sc = new FastScanner();
+	private static final FastPrinter out = new FastPrinter();
+	// endregion
+
+	private static void solve() {
+		int n = sc.nextInt();
+		int[] a = sc.nextInt(n);
+		IntCompressedArray arr = new IntCompressedArray(a);
+		out.println(arr.toArray());
+	}
+
+	// region < Utility Methods >
+	private static boolean isValidRange(final int i, final int from, final int to) {
+		return ((i - from) | (to - 1 - i)) >= 0;
+	}
+
+	private static boolean isValidRange(final int i, final int j, final int h, final int w) {
+		return ((i | j | (h - 1 - i) | (w - 1 - j)) >>> 31) == 0;
+	}
+
+	private static void swap(final char[] a, final int i, final int j) {
+		final char tmp = a[i];
+		a[i] = a[j];
+		a[j] = tmp;
+	}
+
+	private static void swap(final int[] a, final int i, final int j) {
+		final int tmp = a[i];
+		a[i] = a[j];
+		a[j] = tmp;
+	}
+
+	private static void swap(final long[] a, final int i, final int j) {
+		final long tmp = a[i];
+		a[i] = a[j];
+		a[j] = tmp;
+	}
+
+	private static boolean chmin(final char[] a, final int i, final char v) {
+		if (a[i] <= v) return false;
+		a[i] = v;
+		return true;
+	}
+
+	private static boolean chmin(final int[] a, final int i, final int v) {
+		if (a[i] <= v) return false;
+		a[i] = v;
+		return true;
+	}
+
+	private static boolean chmin(final long[] a, final int i, final long v) {
+		if (a[i] <= v) return false;
+		a[i] = v;
+		return true;
+	}
+
+	private static boolean chmax(final char[] a, final int i, final char v) {
+		if (a[i] >= v) return false;
+		a[i] = v;
+		return true;
+	}
+
+	private static boolean chmax(final int[] a, final int i, final int v) {
+		if (a[i] >= v) return false;
+		a[i] = v;
+		return true;
+	}
+
+	private static boolean chmax(final long[] a, final int i, final long v) {
+		if (a[i] >= v) return false;
+		a[i] = v;
+		return true;
+	}
+
+	private static int min(final int a, final int b) {
+		return Math.min(a, b);
+	}
+
+	private static int min(final int a, final int b, final int c) {
+		return Math.min(a, Math.min(b, c));
+	}
+
+	private static int min(int... a) {
+		int len = a.length;
+		int min = a[0];
+		for (int i = 1; i < len; i++) if (min > a[i]) min = a[i];
+		return min;
+	}
+
+	private static int max(final int a, final int b) {
+		return Math.max(a, b);
+	}
+
+	private static int max(final int a, final int b, final int c) {
+		return Math.max(a, Math.max(b, c));
+	}
+
+	private static int max(int... a) {
+		int len = a.length;
+		int max = a[0];
+		for (int i = 1; i < len; i++) if (max < a[i]) max = a[i];
+		return max;
+	}
+
+	private static long min(final long a, final long b) {
+		return Math.min(a, b);
+	}
+
+	private static long min(final long a, final long b, final long c) {
+		return Math.min(a, Math.min(b, c));
+	}
+
+	private static long min(long... a) {
+		int len = a.length;
+		long min = a[0];
+		for (int i = 1; i < len; i++) if (min > a[i]) min = a[i];
+		return min;
+	}
+
+	private static long max(final long a, final long b) {
+		return Math.max(a, b);
+	}
+
+	private static long max(final long a, final long b, final long c) {
+		return Math.max(a, Math.max(b, c));
+	}
+
+	private static long max(long... a) {
+		int len = a.length;
+		long max = a[0];
+		for (int i = 1; i < len; i++) if (max < a[i]) max = a[i];
+		return max;
+	}
+
+	private static double min(final double a, final double b) {
+		return Math.min(a, b);
+	}
+
+	private static double min(final double a, final double b, final double c) {
+		return Math.min(a, Math.min(b, c));
+	}
+
+	private static double min(double... a) {
+		int len = a.length;
+		double min = a[0];
+		for (int i = 1; i < len; i++) if (min > a[i]) min = a[i];
+		return min;
+	}
+
+	private static double max(final double a, final double b) {
+		return Math.max(a, b);
+	}
+
+	private static double max(final double a, final double b, final double c) {
+		return Math.max(a, Math.max(b, c));
+	}
+
+	private static double max(double... a) {
+		int len = a.length;
+		double max = a[0];
+		for (int i = 1; i < len; i++) if (max < a[i]) max = a[i];
+		return max;
+	}
+
+	private static int diff(final int a, final int b) {
+		return a > b ? a - b : b - a;
+	}
+
+	private static long diff(final long a, final long b) {
+		return a > b ? a - b : b - a;
+	}
+
+	private static double diff(final double a, final double b) {
+		return a > b ? a - b : b - a;
+	}
+
+	private static long lModPow(long a, long b, final long mod) {
+		if (b == 0) return 1;
+		long ans = 1;
+		for (a %= mod; b > 1; b >>= 1) {
+			if ((b & 1) == 1) ans = ans * a % mod;
+			a = a * a % mod;
+		}
+		return ans * a % mod;
+	}
+
+	private static int iModPow(int a, int b, final int mod) {
+		if (b == 0) return 1;
+		int ans = 1;
+		for (a %= mod; b > 1; b >>= 1) {
+			if ((b & 1) == 1) ans = (int) ((long) ans * a % mod);
+			a = (int) ((long) a * a % mod);
+		}
+		return (int) ((long) ans * a % mod);
+	}
+
+	private static long lcmLong(final long x, final long y) {
+		return x == 0 || y == 0 ? 0 : x / gcdLong(x, y) * y;
+	}
+
+	private static int lcmInt(final int x, final int y) {
+		return x == 0 || y == 0 ? 0 : x / gcdInt(x, y) * y;
+	}
+
+	private static long gcdLong(long a, long b) {
+		a = abs(a);
+		b = abs(b);
+		if (a == 0) return b;
+		if (b == 0) return a;
+		int commonShift = Long.numberOfTrailingZeros(a | b);
+		a >>= Long.numberOfTrailingZeros(a);
+		while (b != 0) {
+			b >>= Long.numberOfTrailingZeros(b);
+			if (a > b) {
+				long tmp = a;
+				a = b;
+				b = tmp;
+			}
+			b -= a;
+		}
+		return a << commonShift;
+	}
+
+	private static int gcdInt(int a, int b) {
+		a = abs(a);
+		b = abs(b);
+		if (a == 0) return b;
+		if (b == 0) return a;
+		int commonShift = Integer.numberOfTrailingZeros(a | b);
+		a >>= Integer.numberOfTrailingZeros(a);
+		while (b != 0) {
+			b >>= Integer.numberOfTrailingZeros(b);
+			if (a > b) {
+				int tmp = a;
+				a = b;
+				b = tmp;
+			}
+			b -= a;
+		}
+		return a << commonShift;
+	}
+
+	private static void compression(final int[] a, final int len) {
+		final int[] b = copyOf(a, len);
+		sort(b);
+		int n = 1;
+		for (int i = 1; i < len; i++) {
+			if (b[i] == b[i - 1]) continue;
+			b[n++] = b[i];
+		}
+		for (int i = 0; i < len; i++) {
+			a[i] = binarySearch(b, 0, n, a[i]);
+		}
+	}
+
+	private static void compression(final int[][] a, final int n, final int m) {
+		final int len = n * m;
+		final int[] b = new int[len];
+		for (int i = 0, j = 0; i < n; i++, j += m) {
+			System.arraycopy(a[i], 0, b, j, m);
+		}
+		sort(b);
+		int nm = 1;
+		for (int i = 1; i < len; i++) {
+			if (b[i] == b[i - 1]) continue;
+			b[nm++] = b[i];
+		}
+		for (int i = 0; i < n; i++) {
+			int[] ai = a[i];
+			for (int j = 0; j < m; j++) {
+				ai[j] = binarySearch(b, 0, nm, ai[j]);
+			}
+		}
+	}
+
+	private static void compression(final int[][] a, final int n) {
+		int len = 0;
+		for (int i = 0; i < n; i++) len += a[i].length;
+		final int[] b = new int[len];
+		for (int i = 0, j = 0; i < n; i++) {
+			int m = a[i].length;
+			System.arraycopy(a[i], 0, b, j, m);
+			j += m;
+		}
+		sort(b);
+		int nm = 1;
+		for (int i = 1; i < len; i++) {
+			if (b[i] == b[i - 1]) continue;
+			b[nm++] = b[i];
+		}
+		for (int i = 0; i < n; i++) {
+			int[] ai = a[i];
+			for (int j = 0, m = ai.length; j < m; j++) {
+				ai[j] = binarySearch(b, 0, nm, ai[j]);
+			}
+		}
+	}
+
+	private static int digit2(long n) {
+		if (n == 0) return 1;
+		if (n == Long.MIN_VALUE) return 63;
+		return 64 - Long.numberOfLeadingZeros(Math.abs(n));
+	}
+
+	private static int digit2(int n) {
+		if (n == 0) return 1;
+		if (n == Integer.MIN_VALUE) return 31;
+		return 32 - Integer.numberOfLeadingZeros(Math.abs(n));
+	}
+
+	private static int digit10(long n) {
+		if (n == Long.MIN_VALUE) return 19;
+		if (n < 0) n = -n;
+		int res = 0;
+		do {
+			res++;
+			n /= 10;
+		} while (n > 0);
+		return res;
+	}
+
+	private static int digit10(int n) {
+		if (n == Integer.MIN_VALUE) return 10;
+		if (n < 0) n = -n;
+		int res = 0;
+		do {
+			res++;
+			n /= 10;
+		} while (n > 0);
+		return res;
+	}
+
+	private static long lSqrt(final long n) {
+		if (n <= 0) return 0;
+		long x = (long) sqrt(n);
+		while (x * x > n) x--;
+		while ((x + 1) * (x + 1) <= n) x++;
+		return x;
+	}
+
+	private static int iSqrt(final int n) {
+		if (n <= 0) return 0;
+		return (int) sqrt(n);
+	}
+
+	private static void prefix2D(final int[][] a, final IntBinaryOperator op) {
+		for (int[] ai : a) parallelPrefix(ai, op);
+		for (int i = 1, h = a.length, w = a[0].length; i < h; i++) {
+			for (int j = 0; j < w; j++) {
+				a[i][j] = op.applyAsInt(a[i][j], a[i - 1][j]);
+			}
+		}
+	}
+
+	private static void prefix2D(final long[][] a, final LongBinaryOperator op) {
+		for (long[] ai : a) parallelPrefix(ai, op);
+		for (int i = 1, h = a.length, w = a[0].length; i < h; i++) {
+			for (int j = 0; j < w; j++) {
+				a[i][j] = op.applyAsLong(a[i][j], a[i - 1][j]);
+			}
+		}
+	}
+
+	private static int rangeSum(final int[] a, final int i, final int j) {
+		return i > 0 ? a[j] - a[i - 1] : a[j];
+	}
+
+	private static long rangeSum(final long[] a, final int i, final int j) {
+		return i > 0 ? a[j] - a[i - 1] : a[j];
+	}
+
+	private static int rectSum(final int[][] a, final int i1, final int j1, final int i2, final int j2) {
+		int ans = a[i2][j2];
+		if (i1 > 0) ans -= a[i1 - 1][j2];
+		if (j1 > 0) ans -= a[i2][j1 - 1];
+		if (i1 > 0 && j1 > 0) ans += a[i1 - 1][j1 - 1];
+		return ans;
+	}
+
+	private static long rectSum(final long[][] a, final int i1, final int j1, final int i2, final int j2) {
+		long ans = a[i2][j2];
+		if (i1 > 0) ans -= a[i1 - 1][j2];
+		if (j1 > 0) ans -= a[i2][j1 - 1];
+		if (i1 > 0 && j1 > 0) ans += a[i1 - 1][j1 - 1];
+		return ans;
+	}
+	// endregion
+
+	// region < main & debug >
+	public static void main(final String[] args) {
+		try {
+			solve();
+		} finally {
+			out.close();
+		}
+	}
+
+	private static void debugln(final Object... args) {
+		if (DEBUG) {
+			out.flush();
+			if (args == null) System.err.println("null");
+			else if (args.getClass().getComponentType().isArray()) System.err.println(stringify(args));
+			else System.err.println(stream(args).map(Check3::stringify).collect(Collectors.joining("\n", "\n", "")));
+		}
+	}
+
+	private static void debug(final Object... args) {
+		if (DEBUG) {
+			out.flush();
+			if (args == null) System.err.println("null");
+			else if (args.getClass().getComponentType().isArray()) System.err.println(stringify(args));
+			else System.err.println(stream(args).map(Check3::stringify).collect(Collectors.joining(", ", "", "")));
+		}
+	}
+
+	private static String stringify(final Object obj) {
+		return switch (obj) {
+			case null -> "null";
+			case int[][] arr -> "\n" + stream(arr).map(Arrays::toString).collect(Collectors.joining("\n"));
+			case long[][] arr -> "\n" + stream(arr).map(Arrays::toString).collect(Collectors.joining("\n"));
+			case char[][] arr -> "\n" + stream(arr).map(String::valueOf).collect(Collectors.joining("\n"));
+			case Object[][] arr -> "\n" + stream(arr).map(Arrays::deepToString).collect(Collectors.joining("\n"));
+			case int[] arr -> Arrays.toString(arr);
+			case long[] arr -> Arrays.toString(arr);
+			case double[] arr -> Arrays.toString(arr);
+			case char[] arr -> Arrays.toString(arr);
+			case boolean[] arr -> Arrays.toString(arr);
+			case Object[] arr -> deepToString(arr);
+			case Iterable<?> it -> {
+				final StringJoiner sj = new StringJoiner(", ", "[", "]");
+				for (final Object e : it) sj.add(stringify(e));
+				yield sj.toString();
+			}
+			default -> obj.toString();
+		};
+	}
+	// endregion
+}
